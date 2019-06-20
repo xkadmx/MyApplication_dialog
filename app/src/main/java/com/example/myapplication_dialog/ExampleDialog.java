67d0f1bd03cbbf835fc2c852c3898736ba4a2664
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-20 14:34:56 +0200

    adding layout dialot annd a separate class serving AppCompatDialog module

---
+++ app/src/main/java/com/example/myapplication_dialog/ExampleDialog.java
@@ -0,0 +1,6 @@
+package com.example.myapplication_dialog;
+
+import android.support.v7.app.AppCompatDialogFragment;
+
+public class ExampleDialog extends AppCompatDialogFragment {
+}
--- app/src/main/java/com/example/myapplication_dialog/MainActivity.java
+++ app/src/main/java/com/example/myapplication_dialog/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.myapplication_dialog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewUsername;
+    private TextView textViewPassword;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewUsername = findViewById(R.id.textview_username);
+        textViewPassword = findViewById(R.id.textview_password);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+    public void openDialog(){
+
     }
 }
