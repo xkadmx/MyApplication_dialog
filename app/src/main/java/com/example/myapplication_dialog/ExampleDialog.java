f49a8d82610604d2d3c1293e4e4d808aa082a72f
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-20 14:37:29 +0200

    creating AlertDialog.Builder

--- app/src/main/java/com/example/myapplication_dialog/ExampleDialog.java
+++ app/src/main/java/com/example/myapplication_dialog/ExampleDialog.java
@@ -1,6 +1,13 @@
 package com.example.myapplication_dialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
 
 public class ExampleDialog extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog
+    }
 }
